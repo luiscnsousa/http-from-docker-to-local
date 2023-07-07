e6eef1f6a6bc2e68d7cdd70bc4cea408ad38f0bf
Luís Sousa <luiscnsousa@outlook.com>
2023-07-08 00:06:59 +0100

    add api and worker

---
+++ src/Worker/Program.cs
@@ -0,0 +1,27 @@
+﻿using Flurl;
+using Flurl.Http;
+
+namespace Worker
+{
+    public class Program
+    {
+        const string ApiBaseUrl = "http://localhost:55173";
+
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine($"Hello, I'm about to call {ApiBaseUrl}!");
+
+            var response = await ApiBaseUrl
+                .AppendPathSegment("api")
+                .AppendPathSegment("values")
+                .GetJsonAsync<IEnumerable<string>>();
+
+            foreach (var item in response)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Done!");
+        }
+    }
+}
